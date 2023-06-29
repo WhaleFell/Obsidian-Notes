b636306fb670fb57c7a13af51830d36d128515ab
WhaleFell <whalefall9420@outlook.com>
2023-06-29 23:14:26 +0800

    fixx

--- auto-build-summary.py
+++ auto-build-summary.py
@@ -3,57 +3,35 @@
 # Mdbook auto summary
 # summary all .md files in a Mdbook folder
 
-import argparse
-import os
-import re
-from pathlib import Path
-
 import os
 
 
-import os
+def generate_summary(directory, ignore_dirs):
+    with open("SUMMARY.md", "w", encoding="utf-8") as f:
+        f.write("# 落落的Notes\n\n")
 
+        for root, dirs, files in os.walk(directory):
+            dirs[:] = [d for d in dirs if d not in ignore_dirs]
 
-def generate_summary(root_dir, exclude_dirs=None):
-    if exclude_dirs is None:
-        exclude_dirs = []
+            level = root.replace(directory, "").count(os.sep)
+            indent = "  " * level
 
-    summary = []
-
-    for dirpath, dirnames, filenames in os.walk(root_dir):
-        # 排除指定的目录
-        dirnames[:] = [d for d in dirnames if d not in exclude_dirs]
-
-        # 获取当前目录相对于根目录的层级
-        level = dirpath.replace(root_dir, "").count(os.sep)
-        dirpath = dirpath.replace("\\", "/").replace(".", "")
-
-        # 添加当前目录到摘要
-        if level == 0:
-            # summary.append(f"- [{os.path.basename(dirpath)}]({dirpath}/)")
-            pass
-        else:
-            summary.append(
-                f"{'    ' * (level-1)}- [{os.path.basename(dirpath)}]({dirpath}/)"
-            )
-
-        # 添加当前目录下的文件到摘要
-        for filename in filenames:
-            name = os.path.join(dirpath, filename).replace("\\", "/").replace(".", "")
             if level == 0:
-                break
-            if filename.endswith(".md"):
-                summary.append(
-                    f"{'    ' * (level + 1)}- [{os.path.splitext(filename)[0]}]({name})"
-                )
+                title = "落落的Notes"
+            else:
+                title = os.path.basename(root)
 
-    return "\n".join(summary)
+            f.write("{}- [{}]({}/README.md)\n".format(indent, title, root))
+
+            for file in files:
+                if file.endswith(".md") and file != "README.md":
+                    filename = os.path.splitext(file)[0]
+                    f.write("{}  - [{}]({}/{})\n".format(indent, filename, root, file))
 
 
 if __name__ == "__main__":
-    # 设置根目录和要排除的目录
-    root_dir = "."
-    except_dirs = [
+    directory = "."  # 指定要生成 SUMMARY.md 的目录
+    ignore_dirs = [
         ".obsidian",
         ".git",
         ".trash",
@@ -64,14 +42,6 @@ if __name__ == "__main__":
         "book",
         "assets",
         "templates",
-    ]
+    ]  # 指定要忽略的目录列表
 
-    # 生成 SUMMARY.md 文件内容
-    summary_content = generate_summary(root_dir, except_dirs)
-
-    # 将内容写入 SUMMARY.md 文件
-    with open(os.path.join(root_dir, "SUMMARY.md"), "w", encoding="utf8") as f:
-        f.write("# 落落的Notes\n\n")
-        f.write(summary_content)
-
-    print("done!")
+    generate_summary(directory, ignore_dirs)
